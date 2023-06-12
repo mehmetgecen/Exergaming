3ce972d0bda84c60ac8e8bae599ed858e1589288
mehmetgecen <mehmetgecen06@gmail.com>
2023-06-13 01:51:54 +0300

    Puzzle Match Visual Edit

--- Exergame Project/Assets/Scripts/MatchPuzzle.cs
+++ Exergame Project/Assets/Scripts/MatchPuzzle.cs
@@ -7,44 +7,24 @@ using Random = UnityEngine.Random;
 
 public class MatchPuzzle : MonoBehaviour
 {
-    public LevelManager levelManager;
-    public GameObject[] cubes;
-    //public GameObject dirtArea;
-    //public GameObject sponge;
     public bool isMatchCompleted;
-    
     private static int matchCount = 0;
-
     
-    
-    // Start is called before the first frame update
-
-    private void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        cubes[0].GetComponent<MeshRenderer>().material.color = Color.red;
-        cubes[1].GetComponent<MeshRenderer>().material.color = Color.green;
-        cubes[2].GetComponent<MeshRenderer>().material.color = Color.green;
-        cubes[3].GetComponent<MeshRenderer>().material.color = Color.red;
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.GetComponent<MeshRenderer>().material.color == gameObject.GetComponent<MeshRenderer>().material.color && other.gameObject.name =="holdable")
+        if (other.gameObject.CompareTag(gameObject.tag))
         {
             matchCount++;
             other.gameObject.SetActive(false);
             Debug.Log(matchCount);
             
             Debug.Log("First Match Completed.");
-            
         }
         
-        if (matchCount >= 2 && (other.gameObject.GetComponent<MeshRenderer>().material.color != gameObject.GetComponent<MeshRenderer>().material.color) && other.gameObject.name == "holdable")
+
+        if (matchCount >= 2 && (!other.gameObject.CompareTag(gameObject.tag)))
         {
             Debug.Log(other.gameObject.name);
-            
-            //dirtArea.SetActive(true);
-            //sponge.SetActive(true);
             
             gameObject.SetActive(false);
             other.gameObject.SetActive(false);
@@ -55,6 +35,8 @@ public class MatchPuzzle : MonoBehaviour
             
             
         }
+        
+        
     }
     
     
